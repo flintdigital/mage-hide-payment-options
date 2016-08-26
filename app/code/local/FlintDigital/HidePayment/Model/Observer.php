0215828d5d8f1f3be823968413f3463cd78a3e20
Adrian Esquivel <adrianesquivels@gmail.com>
2016-08-26 17:44:18 -0600

    Fix in Observer logic

--- app/code/local/FlintDigital/HidePayment/Model/Observer.php
+++ app/code/local/FlintDigital/HidePayment/Model/Observer.php
@@ -4,12 +4,18 @@ class FlintDigital_HidePayment_Model_Observer
 
     public function paymentMethodIsActive(Varien_Event_Observer $observer)
     {
+        $paymentCodesToHide = explode(",", Mage::getStoreConfig('payment/fdpaymentoverride/method_code'));
+        //For some reason sometimes it gets an empty element
+        if(!empty($paymentCodesToHide) && $paymentCodesToHide[0] == '')
+            unset($paymentCodesToHide[0]);
+
         $instance = $observer->getMethodInstance();
         $result = $observer->getResult();
-        $paymentCodesToHide = explode(",", Mage::getStoreConfig('payment/fdpaymentoverride/method_code'));
 
         //Always available on admin. In frontend is available if not set to be hidden in the config.
-        $result->isAvailable = Mage::app()->getStore()->isAdmin() || !in_array($instance->getCode(), $paymentCodesToHide);
+        if($paymentCodesToHide) {
+            $result->isAvailable = Mage::app()->getStore()->isAdmin() || !in_array($instance->getCode(), $paymentCodesToHide);
+        }
     }
 
 }
